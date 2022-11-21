77da3b47f720d10b22db9fb155a39460c02a16c9
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-21 08:40:51 +0100

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import Auth from "./layouts/Auth";
 import Dashboard from "./layouts/Dashboard";
 import Login from "./pages/Auth/Login";
+import { Routes, Route } from "react-router-dom";
+import RegisterUser from "./pages/Auth/RegisterUser";
+import ResetPasswordForm from "./pages/Auth/ResetPasswordForm";
+import VerifyEmailForm from "./pages/Auth/VerifyEmailForm";
+import ForgotPasswordForm from "./pages/Auth/ForgotPassword";
 
 export default function App() {
-  return <Dashboard children="Main"></Dashboard>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Dashboard children="Main"></Dashboard>}
+        ></Route>
+      </Routes>
+      <Auth>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<RegisterUser />}></Route>
+          <Route path="/reset-password" element={<ResetPasswordForm />}></Route>
+          <Route path="/verify-email" element={<VerifyEmailForm />}></Route>
+          <Route
+            path="/forgot-password"
+            element={<ForgotPasswordForm />}
+          ></Route>
+        </Routes>
+      </Auth>
+    </>
+  );
   // return (
   //   <Auth>
   //     <Login />
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
