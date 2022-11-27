fd07b77e3af29747288ca392cd6a216097478ba9
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-27 07:36:37 +0100

    added transactions

--- src/data/transactions.ts
+++ src/data/transactions.ts
@@ -1,53 +1,87 @@
 export default [
-  {
-    id: 2,
-    account_id: 1,
-    information: {
-      amount: 10.0,
-      reference: "my super cool description",
-      date: "2022-01-02T05:00:21.294000+00:00",
-      subcategory_id: 1,
-      subcategory: {
+    {
         id: 1,
-        label: "Salary",
-        is_income: true,
-        parent_category_id: 1,
-      },
+        account_id: 1,
+        information: {
+            amount: 10.0,
+            reference: "my super cool description",
+            date: "2022-01-05T05:00:21.294000+00:00",
+            subcategory_id: 1,
+            subcategory: {
+                id: 1,
+                label: "Salary",
+                is_income: true,
+                parent_category_id: 1,
+            },
+        },
+        offset_transactions_id: null,
     },
-    offset_transactions_id: null,
-  },
-  {
-    id: 3,
-    account_id: 1,
-    information: {
-      amount: 10.0,
-      reference: "my super cool description",
-      date: "2022-01-02T05:00:21.294000+00:00",
-      subcategory_id: 1,
-      subcategory: {
-        id: 1,
-        label: "Salary",
-        is_income: true,
-        parent_category_id: 1,
-      },
+    {
+        id: 2,
+        account_id: 1,
+        information: {
+            amount: 50.0,
+            reference: "my super cool description",
+            date: "2022-01-06T05:00:21.294000+00:00",
+            subcategory_id: 1,
+            subcategory: {
+                id: 1,
+                label: "Salary",
+                is_income: true,
+                parent_category_id: 1,
+            },
+        },
+        offset_transactions_id: null,
     },
-    offset_transactions_id: null,
-  },
-  {
-    id: 13,
-    account_id: 3,
-    information: {
-      amount: 20.0,
-      reference: "my super cool description",
-      date: "2022-01-02T05:00:21.294000+00:00",
-      subcategory_id: 1,
-      subcategory: {
-        id: 1,
-        label: "Salary",
-        is_income: true,
-        parent_category_id: 1,
-      },
+    {
+        id: 3,
+        account_id: 1,
+        information: {
+            amount: 80.0,
+            reference: "my super cool description",
+            date: "2022-01-08T05:00:21.294000+00:00",
+            subcategory_id: 1,
+            subcategory: {
+                id: 1,
+                label: "Salary",
+                is_income: true,
+                parent_category_id: 1,
+            },
+        },
+        offset_transactions_id: null,
     },
-    offset_transactions_id: 12,
-  },
+    {
+        id: 3,
+        account_id: 1,
+        information: {
+            amount: 10.0,
+            reference: "my super cool description",
+            date: "2022-01-02T05:00:21.294000+00:00",
+            subcategory_id: 1,
+            subcategory: {
+                id: 1,
+                label: "Salary",
+                is_income: true,
+                parent_category_id: 1,
+            },
+        },
+        offset_transactions_id: 13,
+    },
+    {
+        id: 13,
+        account_id: 1,
+        information: {
+            amount: 20.0,
+            reference: "my super cool description",
+            date: "2022-01-12T05:00:21.294000+00:00",
+            subcategory_id: 1,
+            subcategory: {
+                id: 1,
+                label: "Salary",
+                is_income: true,
+                parent_category_id: 1,
+            },
+        },
+        offset_transactions_id: 3,
+    },
 ];
