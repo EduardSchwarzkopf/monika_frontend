2558fec33a8c610a15552fd96bf2522cf7a15d74
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-27 10:19:49 +0100

    renaming

---
+++ src/views/accounting/AccountListView.tsx
@@ -0,0 +1,25 @@
+import accountList from "../../data/accounts";
+import { Stack, Box } from "@chakra-ui/react";
+import { AccountCard } from "../../components/AccountCard";
+
+export default function Accounts() {
+    let totalBalance = 0;
+
+    return (
+        <>
+            <Box w="100%" p={4} mb={12} bg="white">
+                Total: {totalBalance}€
+            </Box>
+            <Stack spacing="4">
+                {accountList.map((item) => (
+                    <AccountCard
+                        id={item.id}
+                        label={item.label}
+                        description={item.description}
+                        balance={item.balance}
+                    />
+                ))}
+            </Stack>
+        </>
+    );
+}
