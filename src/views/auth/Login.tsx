24bd150d004a155d06978246d51c9ea24b656513
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-02 12:09:14 +0100

    added props

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,64 +1,66 @@
 import {
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  Checkbox,
-  Stack,
-  Link,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
 } from "@chakra-ui/react";
 
-function Login() {
-  return (
-    <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-      <Stack align={"center"}>
-        <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-        <Text fontSize={"lg"} color={"gray.600"}>
-          to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
-        </Text>
-      </Stack>
-      <Box
-        rounded={"lg"}
-        bg={useColorModeValue("white", "gray.700")}
-        boxShadow={"lg"}
-        p={8}
-      >
-        <Stack spacing={4}>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" />
-          </FormControl>
-          <Stack spacing={10}>
-            <Stack
-              direction={{ base: "column", sm: "row" }}
-              align={"start"}
-              justify={"space-between"}
-            >
-              <Checkbox>Remember me</Checkbox>
-              <Link color={"blue.400"}>Forgot password?</Link>
+function Login(props) {
+    return (
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+                <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+                <Text fontSize={"lg"} color={"gray.600"}>
+                    to enjoy all of our cool{" "}
+                    <Link color={"blue.400"}>features</Link> ✌️
+                </Text>
             </Stack>
-            <Button
-              bg={"blue.400"}
-              color={"white"}
-              _hover={{
-                bg: "blue.500",
-              }}
+            <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"lg"}
+                p={8}
             >
-              Sign in
-            </Button>
-          </Stack>
+                <Stack spacing={4}>
+                    <FormControl id="email">
+                        <FormLabel>Email address</FormLabel>
+                        <Input type="email" />
+                    </FormControl>
+                    <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <Input type="password" />
+                    </FormControl>
+                    <Stack spacing={10}>
+                        <Stack
+                            direction={{ base: "column", sm: "row" }}
+                            align={"start"}
+                            justify={"space-between"}
+                        >
+                            <Checkbox>Remember me</Checkbox>
+                            <Link color={"blue.400"}>Forgot password?</Link>
+                        </Stack>
+                        <Button
+                            bg={"blue.400"}
+                            color={"white"}
+                            _hover={{
+                                bg: "blue.500",
+                            }}
+                            onClick={props.onLogin}
+                        >
+                            Sign in
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Box>
         </Stack>
-      </Box>
-    </Stack>
-  );
+    );
 }
 
 export default Login;
