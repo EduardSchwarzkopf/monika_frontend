5c976d17e40160118a9efae34a907f89edcbaf32
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-09 13:59:34 +0100

    updated types

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,19 @@
 import { createContext, useContext, useState } from "react";
+import { ChildrenProp } from "../types/ReactTypes";
 
-const AuthContext = createContext();
+type AuthContextType = {
+    isAuthenticated: boolean;
+    updateIsAuthenticated: Function;
+};
+
+const AuthContext = createContext<AuthContextType>({
+    isAuthenticated: false,
+    updateIsAuthenticated: () => {},
+});
 
 const useAuthContext = () => useContext(AuthContext);
 
-const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }: ChildrenProp) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     const updateIsAuthenticated = (value: boolean) => {
