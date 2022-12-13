a3456053d29ab904dfd62501f0c936ce8d401e6d
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-13 14:33:09 +0100

    remove total balance from here

--- src/views/accounting/AccountListView.tsx
+++ src/views/accounting/AccountListView.tsx
@@ -1,10 +1,8 @@
-import { Stack, Box, Spinner } from "@chakra-ui/react";
+import { Stack, Spinner } from "@chakra-ui/react";
 import { AccountCard, AccountCardProps } from "../../components/AccountCard";
 import { useAccountList } from "../../hooks/useAccounts";
 
 export default function Accounts() {
-    let totalBalance = 0;
-
     const { isLoading, data } = useAccountList();
 
     if (isLoading) {
@@ -12,21 +10,16 @@ export default function Accounts() {
     }
 
     return (
-        <>
-            <Box w="100%" p={4} mb={12} bg="white">
-                Total: {totalBalance}€
-            </Box>
-            <Stack spacing="4">
-                {data?.data.map((item: AccountCardProps) => (
-                    <AccountCard
-                        key={item.id}
-                        id={item.id}
-                        label={item.label}
-                        description={item.description}
-                        balance={item.balance}
-                    />
-                ))}
-            </Stack>
-        </>
+        <Stack spacing="4">
+            {data?.data.map((item: AccountCardProps) => (
+                <AccountCard
+                    key={item.id}
+                    id={item.id}
+                    label={item.label}
+                    description={item.description}
+                    balance={item.balance}
+                />
+            ))}
+        </Stack>
     );
 }
