0eeac701a702c7f398566608c492d23e3eafb76f
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-13 14:32:53 +0100

    refactored

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,23 +1,25 @@
+import { useState } from "react";
 import { TransactionsService } from "../service/accounting/TransactionsService";
 import { useBackendApi } from "./useBackendApi";
 
-export const useTransactions = (
-    accountId: number,
-    date_start: Date,
-    date_end: Date,
-    onSuccess: () => void = () => {},
-    onError: () => void = () => {}
-) => {
-    return useBackendApi(
-        "transactions",
+export const useTransactions = (accountId: number) => {
+    const [transactionList, setTransactionList] = useState([]);
+
+    // TODO: get start and end from cookie
+    const date_start = new Date("2022-01-02T05:00:21.294Z");
+    const date_end = new Date("2022-02-01T07:00:21.294Z");
+
+    const { isSuccess, isError, isLoading, error } = useBackendApi(
+        `transactions_${accountId}`,
         () => {
             return TransactionsService.getAll({
-                accountId: accountId,
+                account_id: accountId,
                 date_start: date_start.toISOString(),
                 date_end: date_end.toISOString(),
             });
         },
-        onSuccess,
-        onError
+        (data) => setTransactionList(data?.data)
     );
+
+    return { transactionList, isSuccess, isError, isLoading, error };
 };
