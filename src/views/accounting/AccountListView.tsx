b6e73758d77cbe75c9044accf8c65a69c6df998e
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-14 13:19:56 +0100

    added total card

--- src/views/accounting/AccountListView.tsx
+++ src/views/accounting/AccountListView.tsx
@@ -1,16 +1,40 @@
-import { Stack, Spinner } from "@chakra-ui/react";
+import {
+    Stack,
+    Spinner,
+    Card,
+    CardBody,
+    Text,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { AccountCard, AccountCardProps } from "../../components/AccountCard";
 import { useAccountList } from "../../hooks/useAccounts";
 
 export default function Accounts() {
     const { isLoading, data } = useAccountList();
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        let totalAmount = 0;
+
+        data?.data.forEach((element: AccountCardProps) => {
+            totalAmount += element.balance;
+        });
+
+        setTotal(totalAmount);
+    }, [data]);
 
     if (isLoading) {
         return <Spinner />;
     }
-
     return (
         <Stack spacing="4">
+            <Card bg={useColorModeValue("white", "gray.800")}>
+                <CardBody>
+                    <Text>Total: {total}€</Text>
+                </CardBody>
+            </Card>
+
             {data?.data.map((item: AccountCardProps) => (
                 <AccountCard
                     key={item.id}
