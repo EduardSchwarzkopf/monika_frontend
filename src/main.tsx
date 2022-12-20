4e60a1ce30d563bb312bed2146e4b076e63cfa3c
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-20 08:50:02 +0100

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
